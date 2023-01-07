113913ffbb74112eb902ada6ae6da71a866c4420
João Guedes <joao.guedes.developer@gmail.com>
2023-01-07 17:04:58 -0300

    Dando continuidade

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let body = document.body;
+
 let profile = document.querySelector('.header .flex .profile');
 
 document.querySelector('#user-btn').onclick = () => {
@@ -12,8 +14,24 @@ document.querySelector('#search-btn').onclick = () => {
  profile.classList.remove('active');
 }
 
+let sideBar = document.querySelector('.side-bar');
+
+document.querySelector('#menu-btn').onclick = () => {
+ sideBar.classList.toggle('active');
+ body.classList.toggle('active')
+}
+
+document.querySelector('.side-bar .close-side-bar').onclick = () => {
+ sideBar.classList.remove('active');
+ body.classList.remove('active')
+}
+
+if (window.innerWidth < 1200){
+  sideBar.classList.remove('active');
+  body.classList.remove('active')
+}
+
 window.onscroll = () => {
   profile.classList.remove('active');
- searchForm.classList.remove('active');
-
+  searchForm.classList.remove('active');
 }

344b9050ee69c8034fb1c7c1206999420a4c12c4
João Guedes <joao.guedes.developer@gmail.com>
2023-01-08 10:45:45 -0300

    Criado grido dos cursos e darkmode

--- js/script.js
+++ js/script.js
@@ -35,3 +35,31 @@ window.onscroll = () => {
   profile.classList.remove('active');
   searchForm.classList.remove('active');
 }
+
+let toggleBtn = document.querySelector('#toggle-btn');
+let darkMode = localStorage.getItem('dark-mode');
+
+const enabelDarkMode = () => {
+  toggleBtn.classList.replace('fa-sun', 'fa-moon');
+  body.classList.add('dark');
+  localStorage.setItem('dark-mode', 'enabled')
+}
+
+const disableDarkMode = () => {
+  toggleBtn.classList.replace('fa-moon', 'fa-sun');
+  body.classList.remove('dark');
+  localStorage.setItem('dark-mode', 'disabled')
+}
+
+if (darkMode === 'enabled') {
+  enabelDarkMode();
+}
+
+toggleBtn.onclick = (e) => {
+  let darkMode = localStorage.getItem('dark-mode');
+  if (darkMode === 'disabled') {
+    enabelDarkMode();
+  } else {
+    disableDarkMode();
+  }
+}
